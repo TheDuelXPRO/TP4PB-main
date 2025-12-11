0b1e72b4a9c9d796cd94a9d1735803ce698ba9ae
Gabriel <129912568+TheDuelXPRO@users.noreply.github.com>
2025-12-11 15:42:22 -0300

    Update Cliente.java

--- src/main/java/org/example/suporte/model/Cliente.java
+++ src/main/java/org/example/suporte/model/Cliente.java
@@ -1,4 +1,40 @@
 package org.example.suporte.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @NotBlank
+    @Size(max = 255)
+    @Column(unique = true)
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
